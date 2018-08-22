4980778b27eca8c274bc3ac7bc17e7d46ae5f598
Ubuntu <ubuntu@ip-172-31-39-120.ap-northeast-1.compute.internal>
2018-08-22 06:52:05 +0000

    move hello.js into static/js

---
+++ samples/node/web/vue/hello-vue/static/js/hello.js
@@ -0,0 +1,21 @@
+$(function () {
+    var vm = new Vue({
+        el: '#vm',
+        data: {
+            name: 'Robot',
+            age: 15
+        }
+    });
+    window.vm = vm;
+});
+
+function executeJs() {
+    try {
+        var code = $('#code').val();
+        var fn = new Function('var vm = window.vm;\n' + code);
+        fn();
+    } catch (e) {}
+    return false;
+}
+
+module.exports = executeJs
