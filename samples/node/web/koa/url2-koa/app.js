a1954d776c0ec5bb7eacad2c26989d5d8917c096
allen <277960419@qq.com>
2018-09-17 16:56:59 +0800

    modify jwt

--- samples/node/web/koa/url2-koa/app.js
+++ samples/node/web/koa/url2-koa/app.js
@@ -1,16 +1,31 @@
+import { port, connexionString, secret } from './config/index'
+
 const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
 const controller = require('./controller');
 
-const app = new Koa();
+const jwt = require('koa-jwt');
 
+const errorHandle = require('./middlewares/errorHandle');
+mongoose.connect(connexionString)
+// mongoose promise 风格 [mongoose.Promise = require('bluebird')]
+mongoose.Promise = global.Promise
+
+const app = new Koa();
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
+
+app.use(errorHandle);
+app.use(jwt({
+    secret,
+}).unless({
+    path:[/\/register/, /\/login/],
+}));
 
 // parse request body:
 app.use(bodyParser());
